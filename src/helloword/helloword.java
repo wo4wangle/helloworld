9933c3e789e1754fd578626884bb49183542624b
wo4wangle <476384374@qq.com>
2019-07-10 00:28:09 +0800

    wangle

--- src/helloword/helloword.java
+++ src/helloword/helloword.java
@@ -2,6 +2,6 @@ package helloword;
 
 public class helloword {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello wangle");
     }
 }
